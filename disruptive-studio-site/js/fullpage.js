4d0b32ee7aa191b2b6b787680066d2a0920db74a
Kirby Pagunsan <kirbypagunsan@kirbys-mbp.attlocal.net>
2018-01-09 08:12:32 -0500

    Mobile menu changes, removed fullpage.js on mobile

--- disruptive-studio-site/js/fullpage.js
+++ disruptive-studio-site/js/fullpage.js
@@ -1,21 +1,36 @@
-$(document).ready(function(){
-    $('#fullpage').fullpage({
-    	anchors:['page1'],
-    	controlArrows: false
-    });
-    $('#home, #home-mob, #side-home').click(function() {
-		$.fn.fullpage.moveTo('page1', 0);
-	});
-	$('#bahaari-nav, #work-nav').click(function() {
-		$.fn.fullpage.moveTo('page1', 1);
-	});
-	$('#iswim-nav').click(function() {
-		$.fn.fullpage.moveTo('page1', 2);
-	});
-	$('#harvest-nav').click(function() {
-		$.fn.fullpage.moveTo('page1', 3);
-	});
-	$('#appunt-nav').click(function() {
-		$.fn.fullpage.moveTo('page1', 4);
-	});
+$(document).ready(function() {
+	// Detects if on mobile
+	var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+
+	// If it's on mobile, do this...
+	if (isMobile.matches) {
+	    $("video").remove();
+	}
+	// Else if it's on desktop/tablet, do this...
+	else {
+	    $("#navbar").removeClass("navbar-fixed");
+
+	    // Do fullpage.js on Desktop only
+	    $('#fullpage').fullpage({
+	    	anchors:['page1'],
+	    	controlArrows: false
+	    });
+	    $('#home, #home-mob, #side-home').click(function() {
+			$.fn.fullpage.moveTo('page1', 0);
+		});
+		$('#bahaari-nav, #work-nav').click(function() {
+			$.fn.fullpage.moveTo('page1', 1);
+		});
+		$('#iswim-nav').click(function() {
+			$.fn.fullpage.moveTo('page1', 2);
+		});
+		$('#harvest-nav').click(function() {
+			$.fn.fullpage.moveTo('page1', 3);
+		});
+		$('#appunt-nav').click(function() {
+			$.fn.fullpage.moveTo('page1', 4);
+		});
+	}
+
+	
 });
--- disruptive-studio-site/js/mob-detect.js
+++ disruptive-studio-site/js/mob-detect.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {      
+$(document).ready(function() {      
 	var isMobile = window.matchMedia("only screen and (max-width: 760px)");
 
 	if (isMobile.matches) {
